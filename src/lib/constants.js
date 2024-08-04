c9e0b946f0fdc0a94f8d1a3d7d66d7552c7f70f2
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2024-08-04 21:45:46 +0400

    new updates

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,2 +1,3 @@
 export const BASE_URL = "https://yusifaliyevpro.com";
+export const PREVIEW_URL = process.env.NEXT_PUBLIC_PREVIEW_URL;
 export const isInDevelopment = process.env.NODE_ENV === "development";
