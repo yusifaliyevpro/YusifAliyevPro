30d0c83ecb7ab572896d2d2928c08af2018043a6
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2024-12-15 15:33:34 +0400

    newww

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { MotionConfig } from "framer-motion";
 import { useRouter } from "next/navigation";
 import type { ReactNode } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes/dist";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 
 export function Providers({
   children,
