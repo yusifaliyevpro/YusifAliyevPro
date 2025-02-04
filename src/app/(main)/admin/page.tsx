3f60b0a24b3d5ae15bb07e95219ac9522c3e608f
Yusif Aliyev <114253515+YusifAliyevPro@users.noreply.github.com>
2025-02-04 17:40:43 +0400

    Resend Configuration

--- src/app/(main)/admin/page.tsx
+++ src/app/(main)/admin/page.tsx
@@ -1,14 +1,17 @@
 import AdminLogin from "@/components/AdminLogin";
 import { getAllContacts } from "@/src/lib/actions";
+import { getBlogs } from "@/src/lib/utils";
 import type { Metadata } from "next";
 
 export default async function Admin() {
   const contacts = await getAllContacts();
+  const blogs = await getBlogs({ isEnabled: false });
   return (
     <main className="mt-24 flex min-h-svh flex-col items-center justify-start px-36">
       <AdminLogin
         myPassowrd={process.env.NEXT_SECRET_ADMIN_PASSWORD}
         contacts={contacts}
+        blogs={blogs}
       />
     </main>
   );
--- src/components/AdminLogin.tsx
+++ src/components/AdminLogin.tsx
@@ -1,76 +1,30 @@
 "use client";
-import { cn } from "@/lib/cn";
-import { Input } from "@heroui/input";
+import { Button } from "@heroui/button";
 import { Contact } from "@prisma/client";
-import { useState } from "react";
-import { FaWhatsapp } from "react-icons/fa";
-import { IoCall } from "react-icons/io5";
+import { sendEmail } from "../lib/actions";
+import { BLOGS_QUERYResult } from "@/sanity.types";
 
 export default function AdminLogin({
-  myPassowrd,
-  contacts,
+  // myPassowrd,
+  // contacts,
+  blogs,
 }: {
   myPassowrd: string;
   contacts: Contact[];
+  blogs: BLOGS_QUERYResult;
 }) {
-  const [password, setPassword] = useState("");
   return (
-    <>
-      {password !== myPassowrd ? (
-        <div className="flex flex-col rounded-2xl p-8 py-12 shadow-large">
-          <Input
-            placeholder="Enter Admin Password"
-            label="Admin Login"
-            type="password"
-            className="w-72"
-            value={password}
-            onValueChange={setPassword}
-            labelPlacement="outside"
-          />
-        </div>
-      ) : (
-        <div className="flex min-h-[80svh] w-full flex-col gap-y-3 rounded-2xl p-7 shadow-large">
-          <h1 className="w-full text-center text-2xl font-bold">Admin Panel</h1>
-          <table className="*:border-1" border={1}>
-            <tr className="*:border-1">
-              <th>Full Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Has Whathsap?</th>
-              <th className="w-44">Description</th>
-            </tr>
-            {contacts.map((contact) => (
-              <tr
-                key={contact.id}
-                className={cn("*:border-1 *:py-1", {
-                  "*:font-bold": !contact.isCalled,
-                })}
-              >
-                <td>{contact.fullName}</td>
-                <td>{contact.email}</td>
-                <td className="text-nowrap">{contact.phone}</td>
-                <td>
-                  {contact.hasWhatsApp ? (
-                    <FaWhatsapp className="text-2xl text-green-500" />
-                  ) : (
-                    <IoCall className="text-2xl text-blue-500" />
-                  )}
-                </td>
-                <td className="line-clamp-1 w-44 truncate border-none">
-                  {contact.description}
-                </td>
-              </tr>
-            ))}
-          </table>
-          {/* {contacts.map((contact) => (
-            <div className="flex flex-row gap-x-5 overflow-x-scroll scrollbar-hide">
-              <p className="min-w-32 text-nowrap">{contact.fullName}</p>
-              <p>{contact.email}</p>
-              <p>{contact.description}</p>
-            </div>
-          ))} */}
-        </div>
-      )}
-    </>
+    <main className="flex size-full min-h-svh flex-col items-center justify-center">
+      <section className="flex size-full min-h-72 flex-col items-center justify-center space-y-10 rounded-2xl shadow-medium">
+        {blogs.map((blog, i) => (
+          <div key={i} className="flex flex-row items-center gap-x-10">
+            <p>{blog.title}</p>
+            <Button color="primary" onPress={() => sendEmail(blog.slug)}>
+              Send Email
+            </Button>
+          </div>
+        ))}
+      </section>
+    </main>
   );
 }
